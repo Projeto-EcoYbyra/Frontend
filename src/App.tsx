78b8ca7af5b216710ab71fcdc9e8930f6d5aa529
pedrohenrique096 <moreiraspedro96@gmail.com>
2023-10-06 16:59:31 -0300

    Atualização das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
-import { Link} from 'react-router-dom'
-import React from 'react'
+import React from 'react';
+import './App.css';
+import Home from './pages/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login';
 
-
-function Navbar() {
- 
-  
-
+function App() {
   return (
     <>
-     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
-          <div className="container flex justify-between text-lg">
-            <div className='text-2xl font-bold uppercase'>Eco Ybyra</div>
-
-            <div className='flex gap-4'>
-              <Link to='/login' className='hover:underline'>Login</Link>
-              <Link to='/home' className='hover:underline'>Home</Link>
-              <div className='hover:underline'>Produtos</div>
-              <div className='hover:underline'>Categoria</div>
-              <div className='hover:underline'>Cadastrar</div>
-              <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
-            </div>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
           </div>
-        </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  )
+);
 }
-
-export default Navbar
+export default App;
