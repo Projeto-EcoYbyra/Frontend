c392b1da80a849b63ea53bf9a3eb8e84793ec2ad
pedrohenrique096 <moreiraspedro96@gmail.com>
2023-10-06 16:47:26 -0300

    Criação das Rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/home/Home';
-import Navbar from './components/navBar/NavBar';
-import Footer from './components/footer/Footer';
+import { Link} from 'react-router-dom'
+import React from 'react'
 
-function App() {
+
+function Navbar() {
+ 
+  
+
   return (
     <>
-    <Navbar/>
-    <Home/>
-    <Footer/>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <div className='text-2xl font-bold uppercase'>Eco Ybyra</div>
+
+            <div className='flex gap-4'>
+              <Link to='/login' className='hover:underline'>Login</Link>
+              <Link to='/home' className='hover:underline'>Home</Link>
+              <div className='hover:underline'>Produtos</div>
+              <div className='hover:underline'>Categoria</div>
+              <div className='hover:underline'>Cadastrar</div>
+              <div className='hover:underline'>Perfil</div>
+              <div className='hover:underline'>Sair</div>
+            </div>
+          </div>
+        </div>
     </>
-);
+  )
 }
-export default App;
+
+export default Navbar
