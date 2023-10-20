9d4c71bc71f884f96044eac75c5436c3baadc9e3
unknown <moraes.compras@hotmail.com>
2023-10-20 15:19:45 -0300

    Upload pequenas alterações na pagina login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -38,8 +38,8 @@ function login(e: ChangeEvent<HTMLFormElement>) {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-        <form className="flex justify-center items-center flex-col w-1/2 gap-4" onSubmit={login}>
+      <div className="grid grid-cols-1 lg:grid-cols-1 h-screen place-items-center font-bold fundoLogin">
+        <form className="flex justify-center items-center flex-col w-1/2 gap-4 efvidro" onSubmit={login}>
           <h2 className="text-5xl ">Entrar</h2>
           <div className="flex flex-col w-full">
             <label htmlFor="usuario">Usuário</label>
@@ -85,7 +85,7 @@ function login(e: ChangeEvent<HTMLFormElement>) {
             </Link>
           </p>
         </form>
-        <div className="fundoLogin hidden lg:block"></div>
+        {/* <div className="fundoLogin hidden lg:block"></div> */}
       </div>
     </>
   );
